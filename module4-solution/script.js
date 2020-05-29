4678cb0017e79dcf40783a5995d5ee595d6656b1
AbigailChants <65059969+AbigailChants@users.noreply.github.com>
2020-05-29 16:21:02 -0500

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -32,7 +32,7 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 (function () {
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim", "Abigail", "Ricardo"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim", "James", "Abigail"];
 
 // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Loop over the names array and say either 'Hello' or "Good Bye"
