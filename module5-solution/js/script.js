277866b76465cb1c99e9b2de1b2f2e2eb0a34fed
AbigailChants <65059969+AbigailChants@users.noreply.github.com>
2020-06-05 19:20:27 -0500

    Update script.js

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -110,17 +110,18 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
+    var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
+     insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-     var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
-     insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+     
    },
    false);  // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
-
+// *** finish **
 
 // Given array of category objects, returns a random category object.
 function chooseRandomCategory (categories) {
